2edd9b9504a22af1be56be297a7277c78fbe99b2
marioomr <rbl55544@educastur.es>
2025-09-30 11:10:45 +0200

    EJ3

---
+++ 1ºEv/EJ03/EJ3.3/biblioteca.js
@@ -0,0 +1,25 @@
+const libros = [
+  { id: 1, titulo: "Cien años de soledad", autor: "Gabriel García Márquez", paginas: 417 },
+  { id: 2, titulo: "1984", autor: "George Orwell", paginas: 328 },
+  { id: 3, titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes", paginas: 863 },
+  { id: 4, titulo: "El principito", autor: "Antoine de Saint-Exupéry", paginas: 96 },
+  { id: 5, titulo: "Crónica de una muerte anunciada", autor: "Gabriel García Márquez", paginas: 122 },
+  { id: 6, titulo: "Fahrenheit 451", autor: "Ray Bradbury", paginas: 194 },
+  { id: 7, titulo: "Orgullo y prejuicio", autor: "Jane Austen", paginas: 279 },
+  { id: 8, titulo: "La sombra del viento", autor: "Carlos Ruiz Zafón", paginas: 565 },
+  { id: 9, titulo: "El nombre de la rosa", autor: "Umberto Eco", paginas: 512 },
+  { id: 10, titulo: "Rebelión en la granja", autor: "George Orwell", paginas: 112 }
+];
+
+
+function agregarLibro(nuevoLibro) {
+  libros.push(nuevoLibro);
+}
+
+
+function obtenerLibros() {
+  return libros;
+}
+
+
+export { agregarLibro, obtenerLibros };
