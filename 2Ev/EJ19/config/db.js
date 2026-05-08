c44f591445f91492d578876423277daed7828048
marioomr <rbl55544@educastur.es>
2026-05-08 11:46:11 +0200

    Arreglos en db.js

--- 2Ev/EJ19/config/db.js
+++ 2Ev/EJ19/config/db.js
@@ -1,20 +1,17 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log('Error DB:', err);
-  } else {
-    console.log('MySQL Connected');
-  }
-});
+console.log('MySQL Pool Connected');
 
-module.exports = connection;
+module.exports = pool;
