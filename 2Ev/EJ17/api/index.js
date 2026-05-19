81ab41a88a74f9306be4a19dfaaee8969f195007
marioomr <rbl55544@educastur.es>
2026-05-19 09:20:16 +0200

    vercel

--- 2Ev/EJ17/api/index.js
+++ 2Ev/EJ17/api/index.js
@@ -6,24 +6,25 @@ const path = require('path');
 const app = express();
 
 const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
+  path.join(__dirname, '../access.log'),
   { flags: 'a' }
 );
 
 app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.set('view engine', 'ejs');
 
-app.use(require('./album/album.routes'));
-app.use(require('./artista/artista.routes'));
+app.set('views', path.join(__dirname, '../views'));
+
+app.use(require('../album/album.routes'));
+app.use(require('../artista/artista.routes'));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor en http://localhost:3000');
-});
+module.exports = app;
